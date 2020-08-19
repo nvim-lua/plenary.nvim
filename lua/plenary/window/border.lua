2131727a1acb178f4ababed9c6b01d4ac7d1d275
TJ DeVries <devries.timothyj@gmail.com>
2020-08-18 20:46:38 -0400

    Add border updates from popup

--- lua/plenary/window/border.lua
+++ lua/plenary/window/border.lua
@@ -1,50 +1,79 @@
-package.loaded['plenary.window.border'] = nil
-
 local tbl = require('plenary.tbl')
 
 local Border = {}
 
 Border.__index = Border
 
+Border._default_thickness = {
+  top = 1,
+  right = 1,
+  bot = 1,
+  left = 1,
+}
+
 function Border._create_lines(content_win_options, border_win_options)
   -- TODO: Handle border width, which I haven't right here.
-  local topline
+  local thickness = border_win_options.border_thickness
+
+  local top_enabled = thickness.top == 1
+  local right_enabled = thickness.right == 1
+  local bot_enabled = thickness.bot == 1
+  local left_enabled = thickness.left == 1
+
+  local border_lines = {}
+
+  local topline = nil
+
+  local topleft = (left_enabled and border_win_options.topleft) or ''
+  local topright = (right_enabled and border_win_options.topright) or ''
+
   if border_win_options.title then
     local title = string.format(" %s ", border_win_options.title)
     local title_len = string.len(title)
 
-    local midpoint = math.floor(content_win_options.width / 2) local left_start = midpoint - math.floor(title_len / 2)
+    local midpoint = math.floor(content_win_options.width / 2)
+    local left_start = midpoint - math.floor(title_len / 2)
 
     topline = string.format("%s%s%s%s%s",
-      border_win_options.topleft,
+      topleft,
       string.rep(border_win_options.top, left_start),
       title,
       string.rep(border_win_options.top, content_win_options.width - title_len - left_start),
-      border_win_options.topright
+      topright
     )
   else
-    topline = border_win_options.topleft
-      .. string.rep(border_win_options.top, content_win_options.width)
-      .. border_win_options.topright
+    if top_enabled then
+      topline = topleft
+        .. string.rep(border_win_options.top, content_win_options.width)
+        .. topright
+    end
   end
 
-  local border_lines = { topline }
+  if topline then
+    table.insert(border_lines, topline)
+  end
 
-  local middle_line = (
-    border_win_options.left
-    .. string.rep(' ', content_win_options.width)
-    .. border_win_options.right
+  local middle_line = string.format(
+    "%s%s%s",
+    (left_enabled and border_win_options.left) or '',
+    string.rep(' ', content_win_options.width),
+    (right_enabled and border_win_options.right) or ''
   )
 
   for _ = 1, content_win_options.height do
     table.insert(border_lines, middle_line)
   end
 
-  table.insert(border_lines,
-    border_win_options.botleft
-    .. string.rep('═', content_win_options.width)
-    .. border_win_options.botright
-  )
+  if bot_enabled then
+    table.insert(border_lines,
+      string.format(
+        "%s%s%s",
+        (left_enabled and border_win_options.botleft) or '',
+        string.rep(border_win_options.bot, content_win_options.width),
+        (right_enabled and border_win_options.botright) or ''
+      )
+    )
+  end
 
   return border_lines
 end
@@ -52,8 +81,9 @@ end
 function Border:new(content_bufnr, content_win_id, content_win_options, border_win_options)
   assert(type(content_win_id) == 'number', "Must supply a valid win_id. It's possible you forgot to call with ':'")
 
+  -- TODO: Probably can use just deep_extend, now that it's available
   border_win_options = tbl.apply_defaults(border_win_options, {
-    width = 1,
+    border_thickness = Border._default_thickness,
 
     -- Border options, could be passed as a list?
     topleft  = '╔',
@@ -63,7 +93,7 @@ function Border:new(content_bufnr, content_win_id, content_win_options, border_w
     right    = '║',
     botleft  = '╚',
     botright = '╝',
-    bottom   = '═',
+    bot      = '═',
   })
 
   local obj = {}
@@ -79,26 +109,43 @@ function Border:new(content_bufnr, content_win_id, content_win_options, border_w
   obj.contents = Border._create_lines(content_win_options, border_win_options)
   vim.api.nvim_buf_set_lines(obj.bufnr, 0, -1, false, obj.contents)
 
-  local border_width = border_win_options.width
+  local thickness = border_win_options.border_thickness
 
   obj.win_id = vim.api.nvim_open_win(obj.bufnr, false, {
+    anchor = content_win_options.anchor,
     relative = content_win_options.relative,
     style = "minimal",
-    row = content_win_options.row - border_width,
-    col = content_win_options.col - border_width,
-    width = content_win_options.width + 2 * border_width,
-    height = content_win_options.height + 2 * border_width,
+    row = content_win_options.row - thickness.top,
+    col = content_win_options.col - thickness.left,
+    width = content_win_options.width + thickness.left + thickness.right,
+    height = content_win_options.height + thickness.top + thickness.bot,
   })
 
-  local silent = true
-  vim.cmd(
-    string.format(
-      "autocmd WinLeave,BufLeave,BufDelete <buffer=%s> ++once ++nested %s call nvim_win_close(%s, v:true)",
-      content_bufnr,
-      (silent and "silent!") or "",
-      obj.win_id
-    )
-  )
+  -- local silent = true
+  -- vim.cmd(
+  --   string.format(
+  --     "autocmd WinLeave,BufLeave,BufDelete %s <buffer=%s> ++once ++nested :call popup#close_win(%s, v:true)",
+  --     (silent and "<silent>") or "",
+  --     content_bufnr,
+  --     obj.win_id
+  --   )
+  -- )
+  -- vim.cmd(string.format(
+  --   "autocmd WinClosed,BufLeave,BufDelete,WinLeave <silent> <buffer=%s> ++once ++nested :call popup#close_win(%s, v:true)",
+  --   content_bufnr,
+  --   obj.win_id
+  -- ))
+  vim.cmd(string.format(
+    "autocmd BufLeave,BufDelete <buffer=%s> ++nested ++once :call popup#close_related_win(%s, %s)",
+    content_bufnr,
+    content_win_id,
+    obj.win_id))
+
+  vim.cmd(string.format(
+    "autocmd WinClosed,WinLeave <buffer=%s> ++nested ++once :call popup#close_win(%s, v:true)",
+    content_bufnr,
+    obj.win_id))
+
 
   setmetatable(obj, Border)
 
