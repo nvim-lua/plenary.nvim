0ebdbf32eb65a89eff2b49e03e4d9cb508c5b75f
Simon Hauser <Simon-Hauser@outlook.de>
2021-02-09 20:26:54 +0100

    feat: busted before_each and after_each (#50)

--- tests/plenary/simple_busted_spec.lua
+++ tests/plenary/simple_busted_spec.lua
@@ -1,3 +1,5 @@
+local eq = assert.are.same
+
 local tester_function = function()
   error(7)
 end
@@ -19,5 +21,83 @@ describe('busted specs', function()
 
   pending("Other thing", function()
     error()
+  end)
+
+  describe('befor each', function()
+    local a = 2
+    local b = 3
+    it('is not cleared', function()
+      eq(2, a)
+      eq(3, b)
+      a = a + 1
+      b = b + 1
+    end)
+    describe('nested', function()
+      before_each(function() a = 0 end)
+      it('should clear a but not b', function()
+        eq(0, a)
+        eq(4, b)
+        a = a + 1
+        b = b + 1
+      end)
+      describe('nested nested', function()
+        before_each(function() b = 0 end)
+        it('should clear b as well', function()
+          eq(0, a)
+          eq(0, b)
+          a = a + 1
+          b = b + 1
+        end)
+      end)
+      it('should only clear a', function()
+        eq(0, a)
+        eq(1, b)
+        a = a + 1
+        b = b + 1
+      end)
+    end)
+    it('should clear nothing', function()
+      eq(1, a)
+      eq(2, b)
+    end)
+  end)
+
+  describe('after each', function()
+    local a = 2
+    local b = 3
+    it('is not cleared', function()
+      eq(2, a)
+      eq(3, b)
+      a = a + 1
+      b = b + 1
+    end)
+    describe('nested', function()
+      after_each(function() a = 0 end)
+      it('should not clear any at this point', function()
+        eq(3, a)
+        eq(4, b)
+        a = a + 1
+        b = b + 1
+      end)
+      describe('nested nested', function()
+        after_each(function() b = 0 end)
+        it('should have cleared a', function()
+          eq(0, a)
+          eq(5, b)
+          a = a + 1
+          b = b + 1
+        end)
+      end)
+      it('should have cleared a and b', function()
+        eq(0, a)
+        eq(0, b)
+        a = a + 1
+        b = b + 1
+      end)
+    end)
+    it('should only have cleared a', function()
+      eq(0, a)
+      eq(1, b)
+    end)
   end)
 end)
