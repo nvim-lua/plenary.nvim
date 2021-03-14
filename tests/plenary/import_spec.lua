94e5a3cbcb95e0f8601d0ffd62d9aa7e747d578d
Brian Shu <littlebubu.shu@gmail.com>
2021-03-14 17:12:36 -0400

    added more tests

--- tests/plenary/import_spec.lua
+++ tests/plenary/import_spec.lua
@@ -1,17 +1,15 @@
-local eq = assert.are.same
+local import = require('plenary.import')
+
+local eq = assert.are
 
 describe('import', function()
   it('should be able to glob import', function()
-    local import = require('plenary.import')
-
     import { "*", from = "plenary.filetype" }
 
     assert(type(_get_extension_parts), "function")
   end)
 
   it('should be able to import from local module', function()
-    local import = require('plenary.import')
-
     local M = {}
     function M.hello() return 'hello' end
 
@@ -21,8 +19,6 @@ describe('import', function()
   end)
 
   it('should be able to import into a local function', function()
-    local import = require('plenary.import')
-
     local M = {}
     function M.hello() return 'hello' end
 
@@ -36,11 +32,22 @@ describe('import', function()
   end)
 
   it('should be able to import with a namespace', function()
-    local import = require('plenary.import')
-
     import { from = "plenary.filetype", as = "ft" }
 
     assert(type(ft) == "table")
     assert(type(ft._get_extension_parts), "function")
+  end)
+
+  it('should be able to import only one thing', function()
+    local M = {}
+    function M.hello() return 'hello' end
+    function M.dont_import() end
+
+    import { "hello", from = M, as = "module"}
+
+    eq(type(hello), 'function')
+    eq(hello(), 'hello')
+    eq(type(dont_import), 'nil')
+    eq(type(module), 'table')
   end)
 end)
