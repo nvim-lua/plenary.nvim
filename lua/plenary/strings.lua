f3e38e5c03687f2c81ff2692ce90cf6673520158
delphinus <me@delphinus.dev>
2021-03-15 19:07:03 +0900

    Fallback to vimL when not JIT but in fast loop

--- lua/plenary/strings.lua
+++ lua/plenary/strings.lua
@@ -18,12 +18,15 @@ M.strdisplaywidth = (function()
     end
   else
     return function(str, col)
-      return #str - (col or 0)
+      if vim.in_fast_loop() then
+        return #str - (col or 0)
+      end
+      return vim.fn.strdisplaywidth(str, col)
     end
   end
 end)()
 
-M.utf_ptr2len = (function()
+M.strcharpart = (function()
   if jit and path.sep ~= [[\]] then
     local ffi = require('ffi')
     ffi.cdef[[
@@ -31,58 +34,61 @@ M.utf_ptr2len = (function()
       int utf_ptr2len(const char_u *const p);
     ]]
 
-    return function(str)
+    local function utf_ptr2len(str)
       local c_str = ffi.new('char[?]', #str + 1)
       ffi.copy(c_str, str)
       return ffi.C.utf_ptr2len(c_str)
     end
+
+    return function(str, nchar, charlen)
+      local nbyte = 0
+      if nchar > 0 then
+        while nchar > 0 and nbyte < #str do
+          nbyte = nbyte + utf_ptr2len(str:sub(nbyte + 1))
+          nchar = nchar - 1
+        end
+      else
+        nbyte = nchar
+      end
+
+      local len = 0
+      if charlen then
+        while charlen > 0 and nbyte + len < #str do
+          local off = nbyte + len
+          if off < 0 then
+            len = len + 1
+          else
+            len = len + utf_ptr2len(str:sub(off + 1))
+          end
+          charlen = charlen - 1
+        end
+      else
+        len = #str - nbyte
+      end
+
+      if nbyte < 0 then
+        len = len + nbyte
+        nbyte = 0
+      elseif nbyte > #str then
+        nbyte = #str
+      end
+      if len < 0 then
+        len = 0
+      elseif nbyte + len > #str then
+        len = #str - nbyte
+      end
+
+      return str:sub(nbyte + 1, nbyte + len)
+    end
   else
-    return function(str)
-      return str == '' and 0 or 1
+    return function(str, nchar, charlen)
+      if vim.in_fast_loop() then
+        return str:sub(nchar + 1, charlen)
+      end
+      return vim.fn.strcharpart(str, nchar, charlen)
     end
   end
 end)()
-
-M.strcharpart = function(str, nchar, charlen)
-  local nbyte = 0
-  if nchar > 0 then
-    while nchar > 0 and nbyte < #str do
-      nbyte = nbyte + M.utf_ptr2len(str:sub(nbyte + 1))
-      nchar = nchar - 1
-    end
-  else
-    nbyte = nchar
-  end
-
-  local len = 0
-  if charlen then
-    while charlen > 0 and nbyte + len < #str do
-      local off = nbyte + len
-      if off < 0 then
-        len = len + 1
-      else
-        len = len + M.utf_ptr2len(str:sub(off + 1))
-      end
-      charlen = charlen - 1
-    end
-  else
-    len = #str - nbyte
-  end
-
-  if nbyte < 0 then
-    len = len + nbyte
-    nbyte = 0
-  elseif nbyte > #str then
-    nbyte = #str
-  end
-  if len < 0 then
-    len = 0
-  elseif nbyte + len > #str then
-    len = #str - nbyte
-  end
-
-  return str:sub(nbyte + 1, nbyte + len)
-end
 
 M.truncate = function(str, len, dots)
   str = tostring(str) -- We need to make sure its an actually a string and not a number
