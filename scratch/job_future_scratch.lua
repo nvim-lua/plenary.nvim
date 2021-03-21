310b3d7bf93ddccdff437c6989b1352c8d27aa46
Brian Shu <littlebubu.shu@gmail.com>
2021-03-21 11:39:51 -0400

    updated scratch

--- scratch/job_future_scratch.lua
+++ scratch/job_future_scratch.lua
@@ -1,31 +1,45 @@
 local a = require("plenary.async_lib")
-local uv = vim.loop
 local async, await = a.async, a.await
 local Job = require("plenary.job_future").Job
 
-local fn = async(function()
-  -- local handle = Job { "sleep", "2" }:spawn()
-  -- local res = await(handle:stop())
-  local output = await(Job { "sleep", "2" }:output())
-  dump(output)
-
-  -- dump(handle)
-  -- dump(res)
-  -- assert(res:success())
-end)
-
 local rg = async(function()
   local handle = Job { "rg", "--vimgrep", ".*", cwd = "/home/brian" }:spawn { raw_read = true }
-  for i = 1, 2000 do
+  for i = 1, 1000000 do
     print(await(handle:raw_read("stdout")))
-    await(a.util.sleep(5))
   end
-  await(a.util.sleep(2000))
   await(handle:stop())
 end)
 
-local fd = async(function()
-  local output = await(Job { "fd", cwd = "/home/brian" }:output())
+local rg_slow = function()
+  local Job = require('plenary.job')
+  Job:new {
+    command = "rg",
+    args = {
+      "--vimgrep", ".*"
+    },
+    cwd = "/home/brian",
+  }:start()
+end
+
+local long_job = async(function()
+  local handle = Job { "cat", "-" }:spawn()
+  local output = await(handle:stop())
+  dump(output)
 end)
 
+local long_job_slow = function()
+  local Job = require('plenary.job')
+  local job = Job:new {
+    command = "cat",
+    args = {
+      "-",
+    },
+  }
+  job:start()
+  job:shutdown()
+end
+
+-- rg_slow()
 a.run(rg())
+-- a.run(long_job())
+-- long_job_slow()
