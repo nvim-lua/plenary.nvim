21c6a18d3d0d5ebdf2998883d94d8f31bf26c4a0
Joakker <joaquinandresleon108@gmail.com>
2021-04-15 10:56:29 -0400

    Add checks for integrity in enum constructor

--- lua/plenary/enum.lua
+++ lua/plenary/enum.lua
@@ -1,8 +1,18 @@
 local Enum = {}
 
-setmetatable(Enum, Enum)
+function Enum:__eq(other)
+  if getmetatable(other) == nil then return false end
+  for k, v in pairs(self) do
+    if type(k) == 'string' then
+      if v ~= other[k] then return false end
+    else
+      return false
+    end
+  end
+  return true
+end
 
-function Enum:__call(tbl)
+local function make_enum(tbl)
   local result = {}
   local i = 1
   -- Filter out everything that isn't a string, or a table since we only care
@@ -13,18 +23,18 @@ function Enum:__call(tbl)
       i = i + 1
     elseif type(v) == 'table' then
       local name, value = v[1], v[2]
+      if vim.tbl_contains(value) then error 'Enum values must not overlap' end
       result[name] = value
       i = value + 1
     end
   end
-  return vim.tbl_add_reverse_lookup(result)
+  vim.tbl_add_reverse_lookup(result)
+  return setmetatable(result, Enum)
 end
 
-function Enum.is_enum(tbl)
-  for k, v in ipairs(tbl) do
-    if tbl[k] ~= v or tbl[v] ~= k then return false end
-    return true
-  end
+local function is_enum(tbl)
+  return getmetatable(tbl) == Enum
 end
 
-return Enum
+return setmetatable({is_enum = is_enum, make_enum = make_enum},
+                    {__call = make_enum})
