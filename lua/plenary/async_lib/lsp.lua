7fb241672116522c32870d0452f1ccebe721ee75
Brian Shu <littlebubu.shu@gmail.com>
2021-04-16 09:02:13 -0400

    added buf_request_all to async lib

--- lua/plenary/async_lib/lsp.lua
+++ lua/plenary/async_lib/lsp.lua
@@ -2,7 +2,11 @@ local a = require('plenary.async_lib.async')
 
 local M = {}
 
----Same as vim.lsp.buf_request but works with async await
+---This will be deprecated because the callback can be called multiple times.
+---This will give a coroutine error because the coroutine will be resumed multiple times.
+---Please use buf_request_all instead.
 M.buf_request = a.wrap(vim.lsp.buf_request, 4)
+
+M.buf_request_all = a.wrap(vim.lsp.buf_request_all, 4)
 
 return M
