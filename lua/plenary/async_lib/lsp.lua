bea15f264dbefa524faa8d3fdfac146d4c4257ef
Brian Shu <littlebubu.shu@gmail.com>
2021-04-16 09:25:46 -0400

    added async_void

--- lua/plenary/async_lib/lsp.lua
+++ lua/plenary/async_lib/lsp.lua
@@ -7,6 +7,9 @@ local M = {}
 ---Please use buf_request_all instead.
 M.buf_request = a.wrap(vim.lsp.buf_request, 4)
 
-M.buf_request_all = a.wrap(vim.lsp.buf_request_all, 4)
+--This was recently merged into master so we just check if it is there
+if vim.lsp.buf_request_all ~= nil then
+  M.buf_request_all = a.wrap(vim.lsp.buf_request_all, 4)
+end
 
 return M
