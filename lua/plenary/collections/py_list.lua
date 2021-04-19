29e6571cb75ee3c291b8a50bab738f90df140467
Joakker <joaquinandresleon108@gmail.com>
2021-04-19 14:12:38 -0400

    Use emmy documentation guidelines

--- lua/plenary/collections/py_list.lua
+++ lua/plenary/collections/py_list.lua
@@ -1,7 +1,18 @@
--- vim:sw=2
+---@brief [[
+--- This module implements python-like lists. It can be used like so:
+--- <pre>
+---     local List = require 'plenary.collections.py_list'
+---     local l = List{3, 20, 44}
+---     print(l)  -- [3, 20, 44]
+--- </pre>
+---@brief ]]
 local List = {}
 
--- List constructor. Can be used in higher order functions
+---@class List @The base class for all list objects
+
+---List constructor. Can be used in higher order functions
+---@param  tbl table: A list-like table containing the initial elements of the list
+---@return List: A new list object
 function List.new(tbl)
   if type(tbl) == 'table' then
     local len = #tbl
@@ -12,7 +23,9 @@ function List.new(tbl)
   error 'List constructor must be called with table argument'
 end
 
--- Returns true if tbl's metatable is List
+--- Checks whether the argument is a List object
+--- @param tbl table: The object to test
+--- @return boolean: Whether tbl is an instance of List
 function List.is_list(tbl)
   local meta = getmetatable(tbl) or {}
   return meta == List
@@ -63,58 +76,96 @@ function List:__concat(other)
   return result
 end
 
--- Appends the element to the end of the list
+--- Appends the element to the end of the list
+--- @param other any: The object to append
+--- @see List.pop
 function List:push(other)
   self[#self + 1] = other
   self._len = self._len + 1
 end
 
--- Pops the last element off the list and returns it
+--- Pops the last element off the list and returns it
+--- @return any: The (previously) last element from the list
+--- @see List.push
 function List:pop()
   local result = table.remove(self)
   self._len = self._len - 1
   return result
 end
 
--- Inserts other into the specified idx
+--- Inserts other into the specified idx
+--- @param idx number: The index that other will be inserted to
+--- @param other any: The element to insert
+--- @see List.remove
 function List:insert(idx, other)
   table.insert(self, idx, other)
   self._len = self._len + 1
 end
 
--- Removes the element at index i
-function List:remove(i)
-  table.remove(self, i)
+--- Removes the element at index idx and returns it
+--- @param idx number: The index of the element to remove
+--- @return any: The element previously at index idx
+--- @see List.insert
+function List:remove(idx)
   self._len = self._len - 1
+  return table.remove(self, idx)
 end
 
--- Can be used to compare elements with any list-like table. It only checks for
--- shallow equality
+--- Can be used to compare elements with any list-like table. It only checks for
+--- shallow equality
+--- @param other any: The element to test for
+--- @return boolean: True if other is a list object and all it's elements are equal
+--- @see List.deep_equal
 function List:equal(other)
   return self:__eq(other)
 end
 
--- Checks for deep equality between lists
+--- Checks for deep equality between lists. This uses vim.deep_equal for testing
+--- @param other any: The element to test for
+--- @return boolean: True if all elements and their children are equal
+--- @see List.equal
+--- @see vim.deep_equal
 function List:deep_equal(other)
   return vim.deep_equal(self, other)
 end
 
--- Returns a copy of the list with elements between a and b, inclusive
+--- Returns a copy of the list with elements between a and b, inclusive
+--- <pre>
+---     local list = List{1, 2, 3, 4}
+---     local slice = list:slice(2, 3)
+---     print(slice) -- [2, 3]
+--- </pre>
+--- @param a number: The low end of the slice
+--- @param b number: The high end of the slice
+--- @return List: A list with elements between a and b
 function List:slice(a, b)
   return List.new(vim.list_slice(self, a, b))
 end
 
--- Similar to slice, but with every element. It only makes a shallow copy
+--- Similar to slice, but with every element. It only makes a shallow copy
+--- @return List: A slice from 1 to #self, i.e., a complete copy of the list
+--- @see List.deep_copy
 function List:copy()
   return self:slice(1, #self)
 end
 
--- Similar to copy, but makes a deep copy instead
+--- Similar to copy, but makes a deep copy instead
+--- @return List: A deep copy of the object
+--- @see List.copy
+--- @see vim.deep_copy
 function List:deep_copy()
   return vim.deep_copy(self)
 end
 
--- Reverses the list in place and returns it for chaining calls
+--- Reverses the list in place. If you don't want this, you could do something
+--- like this
+--- <pre>
+---     local list = List{1, 2, 3, 4}
+---     local reversed = list:copy():reverse()
+--- </pre>
+--- @return List: The list itself, so you can chain method calls
+--- @see List.copy
+--- @see List.deep_copy
 function List:reverse()
   local n = #self
   local i = 1
@@ -144,14 +195,17 @@ local function backward_list_gen(param, state)
   if v then return state, v end
 end
 
--- Counts the occurrences of e inside the list
+--- Counts the occurrences of e inside the list
+--- @param e any: The element to test for
+--- @return number: The number of occurrences of e
 function List:count(e)
   local count = 0
   for _, v in self:iter() do if e == v then count = count + 1 end end
   return count
 end
 
--- Appends the elements in the given iterator to the list
+--- Appends the elements in the given iterator to the list
+--- @param other table: An iterator object
 function List:extend(other)
   if type(other) == 'table' and getmetatable(other) == itermetatable then
     for _, v in other do self:push(v) end
@@ -160,19 +214,48 @@ function List:extend(other)
   end
 end
 
--- Returns true as soon as it finds an occurence of the given element, otherwise
--- it returns false
+--- Checks whether there is an occurence of the given element in the list
+--- @param e any: The object to test for
+--- @return boolean: True if e is present
 function List:contains(e)
   for _, v in self:iter() do if v == e then return true end end
   return false
 end
 
--- Creates an iterator for the list
+--- Creates an iterator for the list. For example:
+--- <pre>
+---     local list = List{8, 4, 7, 9}
+---     for i, v in list:iter() do
+---         print(i, v)
+---     end
+--- </pre>
+--- Would print:
+--- <pre>
+---     1    8
+---     2    4
+---     3    7
+---     4    9
+--- </pre>
+--- @return table: An iterator object
 function List:iter()
   return Iterator.wrap(forward_list_gen, self, 0)
 end
 
--- Creates a reverse iterator for the list
+--- Creates a reverse iterator for the list. For example:
+--- <pre>
+---     local list = List{8, 4, 7, 9}
+---     for i, v in list:riter() do
+---         print(i, v)
+---     end
+--- </pre>
+--- Would print:
+--- <pre>
+---     4    9
+---     3    7
+---     2    4
+---     1    8
+--- </pre>
+--- @return table: An iterator object
 function List:riter()
   return Iterator.wrap(backward_list_gen, self, #self + 1)
 end
