17e604cb5a9c801651a9422998777f3858f700bd
Brian Shu <littlebubu.shu@gmail.com>
2021-05-03 20:18:18 -0400

    added arify

--- lua/plenary/fun.lua
+++ lua/plenary/fun.lua
@@ -16,6 +16,16 @@ function M.bind(fn, ...)
   end
 end
 
+function M.arify(fn, argc)
+  return function(...)
+    if select('#', ...) ~= argc then
+      error(("Expected %s number of arguments"):format(argc))
+    end
+
+    fn(...)
+  end
+end
+
 function M.create_wrapper(map)
   return function(to_wrap)
     return function(...)
