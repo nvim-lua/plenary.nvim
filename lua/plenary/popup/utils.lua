32153da48fd5cd91fbbbca369fa2420162aafe5d
TJ DeVries <devries.timothyj@gmail.com>
2021-08-10 10:52:24 -0400

    fixup: stylua

--- lua/plenary/popup/utils.lua
+++ lua/plenary/popup/utils.lua
@@ -1,12 +1,15 @@
-
 local utils = {}
 
 utils.bounded = function(value, min, max)
   min = min or 0
   max = max or math.huge
 
-  if min then value = math.max(value, min) end
-  if max then value = math.min(value, max) end
+  if min then
+    value = math.max(value, min)
+  end
+  if max then
+    value = math.min(value, max)
+  end
 
   return value
 end
