e98918a023a27b08f012356e30327f10050fb334
TJ DeVries <devries.timothyj@gmail.com>
2021-08-11 07:58:08 -0400

    fixup: Update test to search for both types of requires

--- tests/plenary/popup_requires_spec.lua
+++ tests/plenary/popup_requires_spec.lua
@@ -10,14 +10,21 @@ describe("plenary.popup", function()
   }
 
   local matches_any_import = function(line)
-    local matched = string.match(line, [[require "(.*)"]])
+    local matched = string.match(line, [[require."(.*)"]])
     if matched and not vim.startswith(matched, "plenary.popup") then
       if not allowed_imports[matched] then
         return true, string.format("Not an allowed import for popup: %s. Line: %s", matched, line)
       end
     end
+
     return false, nil
   end
+
+  -- Tests to make sure that we're matching both types of requires
+  it("should match these kinds of patterns", function()
+    eq(true, matches_any_import [[local x = require "plenary.other"]])
+    eq(true, matches_any_import [[local x = require("plenary.module").something]])
+  end)
 
   it("must not require anything other than Window and Border from plenary", function()
     local result = scandir.scan_dir("./lua/plenary/popup", { depth = 1 })
