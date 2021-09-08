8ef8dad50c8fd0e2c1a59f6ca5fea51e75c0648e
Tami <65782666+tami5@users.noreply.github.com>
2021-09-08 07:38:12 +0300

    make it work with a single function

--- lua/plenary/benchmark/init.lua
+++ lua/plenary/benchmark/init.lua
@@ -20,27 +20,33 @@ local get_stat = function(start, runs, fun)
 end
 
 local get_output = function(name, res)
-  return ('  - ("%s") min: %.3fs, max: %.3fs, mean: %.3fs, median: %fs, std: %fs\n'):format(
-    name,
-    res.min,
-    res.max,
-    res.mean,
-    res.median,
-    res.std
-  )
+  local start = "  - "
+  if type(name) ~= "number" then
+    start = start .. ("[[%s]] "):format(name)
+  end
+
+  return start
+    .. ("min: %.3fs, max: %.3fs, mean: %.3fs, median: %fs, std: %fs\n"):format(
+      res.min,
+      res.max,
+      res.mean,
+      res.median,
+      res.std
+    )
 end
 
 ---@class benchmark_run_opts
 ---@field warmup number @number of initial runs before starting to track time.
 ---@field runs number @number of runs to make
----@field funs table<string, function> @function to execute
+---@field fun table<string, function>|function @function to execute
 
 ---Benchmark a function
 ---@param name string @benchmark name
 ---@param opts benchmark_run_opts
 local bench = function(name, opts)
+  opts.fun = type(opts.fun) == "function" and { opts.fun } or opts.fun
   for _ = 1, opts.warmup or 5 do
-    for _, fun in pairs(opts.funs) do
+    for _, fun in pairs(opts.fun) do
       fun()
     end
   end
@@ -48,7 +54,7 @@ local bench = function(name, opts)
 
   local output, res = "", {}
 
-  for k, fun in pairs(opts.funs) do
+  for k, fun in pairs(opts.fun) do
     res[k] = get_stat(start, opts.runs, fun)
     output = output .. get_output(k, res[k])
   end
@@ -56,11 +62,30 @@ local bench = function(name, opts)
   res.elapsed = vim.loop.hrtime() - start
 
   print(
-    ('("%s") Benchmark: \n\n  - total elapsed time: %.3fms\n  - runs: %s\n'):format(name, res.elapsed, opts.runs)
+    ("[[%s]] Benchmark: \n\n  - total elapsed time: %.3fms\n  - runs: %s\n"):format(name, res.elapsed, opts.runs)
       .. output
   )
 
   return res
 end
+
+-- bench("Predicting Y", {
+--   runs = 10,
+--   fun = function()
+--     vim.wait(200, function() end)
+--   end,
+-- })
+-- (slower / faster)
+bench("Predicting Y", {
+  runs = 5,
+  fun = {
+    base = function()
+      vim.wait(200, function() end)
+    end,
+    ench = function()
+      vim.wait(100, function() end)
+    end,
+  },
+})
 
 return bench
