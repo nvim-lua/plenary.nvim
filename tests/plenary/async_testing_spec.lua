33b3b9f4a00d29cc5053c379552b0245135a60df
Simon Hauser <Simon-Hauser@outlook.de>
2023-01-06 15:18:43 +0100

    fix: stylua

--- tests/plenary/async_testing_spec.lua
+++ tests/plenary/async_testing_spec.lua
@@ -10,7 +10,10 @@ function Timing:check(from, to, min_elapsed)
   assert(self[from], "did not log " .. from)
   assert(self[to], "did not log " .. to)
   local elapsed = self[to] - self[from]
-  assert(min_elapsed <= elapsed, string.format("only took %s to get from %s to %s - expected at least %s", elapsed, from, to, min_elapsed))
+  assert(
+    min_elapsed <= elapsed,
+    string.format("only took %s to get from %s to %s - expected at least %s", elapsed, from, to, min_elapsed)
+  )
 end
 
 describe("Async test", function()
@@ -18,14 +21,14 @@ describe("Async test", function()
     local co = coroutine.running()
     assert(co, "not running inside a coroutine")
 
-    local timing = setmetatable({}, {__index = Timing})
+    local timing = setmetatable({}, { __index = Timing })
 
     vim.defer_fn(function()
       coroutine.resume(co)
     end, 200)
-    timing:log("before")
+    timing:log "before"
     coroutine.yield()
-    timing:log("after")
+    timing:log "after"
     timing:check("before", "after", 0.1)
   end)
 
@@ -33,32 +36,35 @@ describe("Async test", function()
     local co = coroutine.running()
     assert(co, "not running inside a coroutine")
 
-    local timing = setmetatable({}, {__index = Timing})
+    local timing = setmetatable({}, { __index = Timing })
 
-    Job:new {
+    Job:new({
       command = "bash",
-      args = { "-ce", [[
+      args = {
+        "-ce",
+        [[
         sleep 0.2
         echo hello
         sleep 0.2
         echo world
         sleep 0.2
         exit 42
-      ]] },
+      ]],
+      },
       on_stdout = function(_, data)
         timing:log(data)
       end,
       on_exit = function(_, exit_status)
-        timing:log("exit")
+        timing:log "exit"
         --This is required so that the rest of the test will run in a proper context
         vim.schedule(function()
           coroutine.resume(co, exit_status)
         end)
-      end
-    }:start()
-    timing:log("job started")
+      end,
+    }):start()
+    timing:log "job started"
     local exit_status = coroutine.yield()
-    timing:log("job finished")
+    timing:log "job finished"
     assert.are.equal(exit_status, 42)
 
     timing:check("job started", "job finished", 0.3)
