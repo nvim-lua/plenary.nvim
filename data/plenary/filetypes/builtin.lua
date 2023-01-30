9a0d3bf7b832818c042aaf30f692b081ddd58bd9
Mark Skelton <mdskelton99@gmail.com>
2023-01-30 12:20:17 -0600

    feat: add mts, cts, .stylelintrc, .clangd filetypes (#454)

--- data/plenary/filetypes/builtin.lua
+++ data/plenary/filetypes/builtin.lua
@@ -21,6 +21,7 @@ return {
   extension = {
     ['_coffee'] = 'coffee',
     ['astro'] = 'astro',
+    ['cts'] = 'typescript',
     ['cljd'] = 'clojure',
     ['coffee'] = 'coffee',
     ['dart'] = 'dart',
@@ -39,6 +40,7 @@ return {
     ['jl'] = 'julia',
     ['jsx'] = 'javascriptreact',
     ['kt'] = 'kotlin',
+    ['mts'] = 'typescript',
     ['nix'] = 'nix',
     ['plist'] = 'xml',
     ['purs'] = 'purescript',
@@ -55,9 +57,11 @@ return {
   file_name = {
     ['cakefile'] = 'coffee',
     ['.babelrc'] = 'json',
+    ['.clangd'] = 'yaml',
     ['.eslintrc'] = 'json',
     ['.firebaserc'] = 'json',
     ['.prettierrc'] = 'json',
+    ['.stylelintrc'] = 'json',
   },
   shebang = shebang
 }
