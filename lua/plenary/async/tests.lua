1d1517ab0fc38fb12390ea5184a836a8a4bbf3b1
Shih-Ming Wang <swang150@ucsc.edu>
2023-02-19 01:02:19 -0800

    feat: add a.pending. (#451)
    This enables me to do `:1,$ s/it/penging/g` to test a single test when both
    `a.it` and `it` appear in the test file.

--- lua/plenary/async/tests.lua
+++ lua/plenary/async/tests.lua
@@ -10,6 +10,10 @@ M.it = function(s, async_func)
   it(s, util.will_block(async_func, tonumber(vim.env.PLENARY_TEST_TIMEOUT)))
 end
 
+M.pending = function(async_func)
+  pending(async_func)
+end
+
 M.before_each = function(async_func)
   before_each(util.will_block(async_func))
 end
