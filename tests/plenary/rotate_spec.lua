52544825d6b8be677eb395983515ad5e30ae1f2e
Simon Hauser <simon.hauser@helsinki-systems.de>
2023-05-24 17:28:04 +0200

    fix: rotate_spec not correctly asserting
    fixes #494

--- tests/plenary/rotate_spec.lua
+++ tests/plenary/rotate_spec.lua
@@ -1,11 +1,13 @@
 local rotate = require("plenary.vararg").rotate
 
-local eq = vim.deep_equal
+local eq = function(a, b)
+  assert.is["true"](vim.deep_equal(a, b), true)
+end
 
 describe("rotate", function()
   it("should rotate varargs", function()
-    eq({ rotate(1, 2, 3) }, { 2, 3, 1 })
-    eq({ rotate(1, 2, 3, 4, 5, 6, 7, 8, 9) }, { 2, 3, 4, 5, 6, 7, 8, 9, 1 })
+    eq({ rotate(3, 1, 2, 3) }, { 2, 3, 1 })
+    eq({ rotate(9, 1, 2, 3, 4, 5, 6, 7, 8, 9) }, { 2, 3, 4, 5, 6, 7, 8, 9, 1 })
   end)
 
   it("should rotate none", function()
@@ -13,6 +15,6 @@ describe("rotate", function()
   end)
 
   it("should rotate one", function()
-    eq({ rotate(1) }, { 1 })
+    eq({ rotate(1, 1) }, { 1 })
   end)
 end)
