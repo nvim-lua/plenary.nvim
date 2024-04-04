6c10dd2202bee042b422200d9e586607b8a2eb53
delphinus <me@delphinus.dev>
2024-04-04 18:44:37 +0900

    feat: add type annotations for plenary.reload

--- lua/plenary/reload.lua
+++ lua/plenary/reload.lua
@@ -1,5 +1,8 @@
+---@class PlenaryReload
 local reload = {}
 
+---@param module_name string
+---@param starts_with_only? boolean
 reload.reload_module = function(module_name, starts_with_only)
   -- Default to starts with only
   if starts_with_only == nil then
@@ -9,17 +12,22 @@ reload.reload_module = function(module_name, starts_with_only)
   -- TODO: Might need to handle cpath / compiled lua packages? Not sure.
   local matcher
   if not starts_with_only then
+    ---@param pack string
+    ---@return integer?
     matcher = function(pack)
       return string.find(pack, module_name, 1, true)
     end
   else
     local module_name_pattern = vim.pesc(module_name)
+    ---@param pack string
+    ---@return integer?
     matcher = function(pack)
       return string.find(pack, "^" .. module_name_pattern)
     end
   end
 
   -- Handle impatient.nvim automatically.
+  ---@diagnostic disable-next-line: undefined-field
   local luacache = (_G.__luacache or {}).cache
 
   for pack, _ in pairs(package.loaded) do
