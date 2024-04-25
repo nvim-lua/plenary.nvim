08e301982b9a057110ede7a735dd1b5285eb341f
AoMe · 青目 <71392160+AlejandroSuero@users.noreply.github.com>
2024-04-26 00:26:00 +0200

    fix(popup): `set modifiable` failed when `vim.api.nvim_buf_set_lines` (#575)

--- lua/plenary/popup/init.lua
+++ lua/plenary/popup/init.lua
@@ -123,9 +123,7 @@ function popup.create(what, vim_options)
     assert(bufnr, "Failed to create buffer")
 
     vim.api.nvim_buf_set_option(bufnr, "bufhidden", "wipe")
-    if vim.o.modifiable == false then
-      vim.api.nvim_buf_set_option(bufnr, "modifiable", true)
-    end
+    vim.api.nvim_buf_set_option(bufnr, "modifiable", true)
 
     -- TODO: Handle list of lines
     if type(what) == "string" then
