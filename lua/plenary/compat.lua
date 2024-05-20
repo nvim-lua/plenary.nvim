nvim-lua/plenary.nvim

commit ce8556de8fa51c783bee6304cbe012407ea413b9

fix: 0.11 compat (#586)

lua/plenary/curl.lua
@@ -37,6 +37,14 @@ local F = require "plenary.functional"
 local J = require "plenary.job"
 local P = require "plenary.path"
 
+local flatten = function(t)
+  if vim.fn.has "nvim-0.11" == 1 then
+    return vim.iter(t):flatten():totable()
+  else
+    return vim.tbl_flatten(t)
+  end
+end
+
 -- Utils ----------------------------------------------------
 -------------------------------------------------------------
 
@@ -54,7 +62,7 @@ util.url_encode = function(str)
 end
 
 util.kv_to_list = function(kv, prefix, sep)
-  return vim.tbl_flatten(F.kv_map(function(kvp)
+  return flatten(F.kv_map(function(kvp)
     return { prefix, kvp[1] .. sep .. kvp[2] }
   end, kv))
 end
@@ -243,7 +251,7 @@ parse.request = function(opts)
     table.insert(result, { "-o", opts.output })
   end
   table.insert(result, parse.url(opts.url, opts.query))
-  return vim.tbl_flatten(result), opts
+  return flatten(result), opts
 end
 
 -- Parse response ------------------------------------------

lua/plenary/iterators.lua
@@ -38,6 +38,8 @@ function Iterator:__tostring()
   return "<iterator>"
 end
 
+local is_list = vim.islist or vim.tbl_islist
+
 -- A special hack for zip/chain to skip last two state, if a wrapped iterator
 -- has been passed
 local numargs = function(...)
@@ -107,7 +109,7 @@ local rawiter = function(obj, param, state)
       end
     end
 
-    if vim.tbl_islist(obj) then
+    if is_list(obj) then
       return ipairs(obj)
     else
       -- hash

lua/plenary/job.lua
@@ -1,5 +1,6 @@
 local vim = vim
 local uv = vim.loop
+local is_list = vim.islist or vim.tbl_islist
 
 local F = require "plenary.functional"
 
@@ -416,7 +417,7 @@ function Job:_execute()
   if self.writer then
     if Job.is_job(self.writer) then
       self.writer:_execute()
-    elseif type(self.writer) == "table" and vim.tbl_islist(self.writer) then
+    elseif type(self.writer) == "table" and is_list(self.writer) then
       local writer_len = #self.writer
       for i, v in ipairs(self.writer) do
         self.stdin:write(v)

lua/plenary/scandir.lua
@@ -3,6 +3,13 @@ local os_sep = Path.path.sep
 local F = require "plenary.functional"
 
 local uv = vim.loop
+local flatten = function(t)
+  if vim.fn.has "nvim-0.11" == 1 then
+    return vim.iter(t):flatten():totable()
+  else
+    return vim.tbl_flatten(t)
+  end
+end
 
 local m = {}
 
@@ -151,8 +158,8 @@ m.scan_dir = function(path, opts)
   opts = opts or {}
 
   local data = {}
-  local base_paths = vim.tbl_flatten { path }
-  local next_dir = vim.tbl_flatten { path }
+  local base_paths = flatten { path }
+  local next_dir = flatten { path }
 
   local gitignore = opts.respect_gitignore and make_gitignore(base_paths) or nil
   local match_search_pat = opts.search_pattern and gen_search_pat(opts.search_pattern) or nil
@@ -204,8 +211,8 @@ m.scan_dir_async = function(path, opts)
   opts = opts or {}
 
   local data = {}
-  local base_paths = vim.tbl_flatten { path }
-  local next_dir = vim.tbl_flatten { path }
+  local base_paths = flatten { path }
+  local next_dir = flatten { path }
   local current_dir = table.remove(next_dir, 1)
 
   -- TODO(conni2461): get gitignore is not async

tests/plenary/path_spec.lua
@@ -1,5 +1,6 @@
 local Path = require "plenary.path"
 local path = Path.path
+local is_list = vim.islist or vim.tbl_islist
 
 describe("Path", function()
   it("should find valid files", function()
@@ -592,7 +593,7 @@ describe("Path", function()
     it("should extract the ancestors of the path", function()
       local p = Path:new(vim.loop.cwd())
       local parents = p:parents()
-      assert(vim.tbl_islist(parents))
+      assert(is_list(parents))
       for _, parent in pairs(parents) do
         assert.are.same(type(parent), "string")
       end

commit a3e3bc82a3f95c5ed0d7201546d5d2c19b20d683

fix(compat): in fast vim loop issue (#589)
fixes regression introduced in ce8556d

lua/plenary/compat.lua
@@ -1,0 +1,17 @@
+local m = {}
+
+m.flatten = (function()
+  if vim.fn.has "nvim-0.11" == 1 then
+    return function(t)
+      return vim.iter(t):flatten():totable()
+    end
+  else
+    return function(t)
+      return vim.tbl_flatten(t)
+    end
+  end
+end)()
+
+m.islist = vim.islist or vim.tbl_islist
+
+return m

lua/plenary/curl.lua
@@ -36,14 +36,7 @@ local util, parse = {}, {}
 local F = require "plenary.functional"
 local J = require "plenary.job"
 local P = require "plenary.path"
-
-local flatten = function(t)
-  if vim.fn.has "nvim-0.11" == 1 then
-    return vim.iter(t):flatten():totable()
-  else
-    return vim.tbl_flatten(t)
-  end
-end
+local compat = require "plenary.compat"
 
 -- Utils ----------------------------------------------------
 -------------------------------------------------------------
@@ -62,7 +55,7 @@ util.url_encode = function(str)
 end
 
 util.kv_to_list = function(kv, prefix, sep)
-  return flatten(F.kv_map(function(kvp)
+  return compat.flatten(F.kv_map(function(kvp)
     return { prefix, kvp[1] .. sep .. kvp[2] }
   end, kv))
 end
@@ -251,7 +244,7 @@ parse.request = function(opts)
     table.insert(result, { "-o", opts.output })
   end
   table.insert(result, parse.url(opts.url, opts.query))
-  return flatten(result), opts
+  return compat.flatten(result), opts
 end
 
 -- Parse response ------------------------------------------

lua/plenary/iterators.lua
@@ -7,6 +7,7 @@
 
 local co = coroutine
 local f = require "plenary.functional"
+local compat = require "plenary.compat"
 
 --------------------------------------------------------------------------------
 -- Tools
@@ -37,8 +38,6 @@ end
 function Iterator:__tostring()
   return "<iterator>"
 end
-
-local is_list = vim.islist or vim.tbl_islist
 
 -- A special hack for zip/chain to skip last two state, if a wrapped iterator
 -- has been passed
@@ -109,7 +108,7 @@ local rawiter = function(obj, param, state)
       end
     end
 
-    if is_list(obj) then
+    if compat.islist(obj) then
       return ipairs(obj)
     else
       -- hash

lua/plenary/job.lua
@@ -1,6 +1,6 @@
 local vim = vim
 local uv = vim.loop
-local is_list = vim.islist or vim.tbl_islist
+local compat = require "plenary.compat"
 
 local F = require "plenary.functional"
 
@@ -417,7 +417,7 @@ function Job:_execute()
   if self.writer then
     if Job.is_job(self.writer) then
       self.writer:_execute()
-    elseif type(self.writer) == "table" and is_list(self.writer) then
+    elseif type(self.writer) == "table" and compat.islist(self.writer) then
       local writer_len = #self.writer
       for i, v in ipairs(self.writer) do
         self.stdin:write(v)

lua/plenary/scandir.lua
@@ -1,15 +1,9 @@
 local Path = require "plenary.path"
 local os_sep = Path.path.sep
 local F = require "plenary.functional"
+local compat = require "plenary.compat"
 
 local uv = vim.loop
-local flatten = function(t)
-  if vim.fn.has "nvim-0.11" == 1 then
-    return vim.iter(t):flatten():totable()
-  else
-    return vim.tbl_flatten(t)
-  end
-end
 
 local m = {}
 
@@ -158,8 +152,8 @@ m.scan_dir = function(path, opts)
   opts = opts or {}
 
   local data = {}
-  local base_paths = flatten { path }
-  local next_dir = flatten { path }
+  local base_paths = compat.flatten { path }
+  local next_dir = compat.flatten { path }
 
   local gitignore = opts.respect_gitignore and make_gitignore(base_paths) or nil
   local match_search_pat = opts.search_pattern and gen_search_pat(opts.search_pattern) or nil
@@ -211,8 +205,8 @@ m.scan_dir_async = function(path, opts)
   opts = opts or {}
 
   local data = {}
-  local base_paths = flatten { path }
-  local next_dir = flatten { path }
+  local base_paths = compat.flatten { path }
+  local next_dir = compat.flatten { path }
   local current_dir = table.remove(next_dir, 1)
 
   -- TODO(conni2461): get gitignore is not async

tests/plenary/path_spec.lua
@@ -1,6 +1,6 @@
 local Path = require "plenary.path"
 local path = Path.path
-local is_list = vim.islist or vim.tbl_islist
+local compat = require "plenary.compat"
 
 describe("Path", function()
   it("should find valid files", function()
@@ -593,7 +593,7 @@ describe("Path", function()
     it("should extract the ancestors of the path", function()
       local p = Path:new(vim.loop.cwd())
       local parents = p:parents()
-      assert(is_list(parents))
+      assert(compat.islist(parents))
       for _, parent in pairs(parents) do
         assert.are.same(type(parent), "string")
       end